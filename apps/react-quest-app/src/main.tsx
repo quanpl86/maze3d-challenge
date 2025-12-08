47f23bbd14905b1d4debdc84ab5f16a086a4596c
Long Quân <34340053+quanpl86@users.noreply.github.com>
2025-12-08 10:17:50 +0700

    Update Map Buider, gameSolver, Tạo lời giải tự động

--- apps/react-quest-app/src/main.tsx
+++ apps/react-quest-app/src/main.tsx
@@ -1,6 +1,7 @@
 // apps/react-quest-app/src/main.tsx
 
 import React from 'react'
+import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -8,7 +9,7 @@ import './index.css';
 
 // Import file i18n trung tâm của app.
 // Thao tác này sẽ khởi tạo i18next với các tài nguyên đã được hợp nhất.
-import './i18n.ts';
+import i18n from './i18n';
 
 // Import CSS của thư viện (nếu chưa chuyển sang CSS Modules)
 import '@repo/quest-player/index.css';
@@ -16,8 +17,10 @@ import '@repo/quest-player/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </I18nextProvider>
   </React.StrictMode>,
 );
